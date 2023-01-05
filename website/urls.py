90b2557e6a67400486c82115ec26806df6c4b34e
João Guilherme <azazazazaisjoao@gmail.com>
2023-01-05 00:01:46 -0400

    Página de contato Django

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('contact_us.html', views.contact, name="contact"),
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html', {})
+
+def contact(request):
+    return render(request, 'contact_us.html', {})
