c32b8a271d475f31872119976f15071294fcb9d8
João Guilherme <azazazazaisjoao@gmail.com>
2023-03-27 16:02:58 -0400

    Adicionando funcoes para o Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn psico.wsgi
--- psico/settings.py
+++ psico/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'psico.urls'
@@ -118,9 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -134,3 +141,5 @@ EMAIL_HOST_USER = 'azazazazaisjoao@gmail.com'
 EMAIL_HOST_PASSWORD = 'quxfapvzmiasaufo'
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
+
+django_heroku.settings(locals())
